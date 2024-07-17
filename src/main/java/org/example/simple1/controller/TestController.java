82b581d33e84457702fc2eda2046e16b814ddbb9
jiwon <sjwsrh@naver.com>
2024-07-17 17:32:42 +0900

    two commit

--- src/main/java/org/example/simple1/controller/TestController.java
+++ src/main/java/org/example/simple1/controller/TestController.java
@@ -31,7 +31,7 @@ public class TestController {
         memberDto.setAge(30);
         return memberDto;
     }
-    @GetMapping("/test44")
+    @GetMapping("/test444")
     public MemberDto test4() {
         MemberDto memberDto = new MemberDto();
         memberDto.setName("test4");
